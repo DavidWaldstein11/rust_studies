39da149a91d0a035382c52c9b0171c74194627e9
David Waldstein <78954165+DavidWaldstein11@users.noreply.github.com>
2025-04-03 17:41:27 -0300

    Delete rust_studies/rust_training/variable/target directory

--- rust_studies/rust_training/variable/target/debug/deps/variable-81dbc95d7c1b3aae.d
+++
@@ -1,5 +0,0 @@
-C:\Projetos\rust_studies\rust_training\variable\target\debug\deps\variable-81dbc95d7c1b3aae.exe: src\main.rs
-
-C:\Projetos\rust_studies\rust_training\variable\target\debug\deps\variable-81dbc95d7c1b3aae.d: src\main.rs
-
-src\main.rs:
--- rust_studies/rust_training/variable/target/debug/variable.d
+++
@@ -1,1 +0,0 @@
-C:\Projetos\rust_studies\rust_training\variable\target\debug\variable.exe: C:\Projetos\rust_studies\rust_training\variable\src\main.rs
