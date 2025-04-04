3bc865639ca7bf3d0f76e6d8853e08632d5ba054
David Waldstein <78954165+DavidWaldstein11@users.noreply.github.com>
2025-04-04 17:52:40 -0300

    Update main.rs

--- rust_studies/rust_training/exercise1/src/main.rs
+++ rust_studies/rust_training/exercise1/src/main.rs
@@ -6,10 +6,21 @@ fn main() {
     let name: &str = "John";
 
     let year_of_birth: u16 = 1990;
-    let birth_month: u16 = 12;
-    let current_year: u16 = 2025;
+    let birth_month: u16 = 1;
+    let birth_day: u16 = 31;
 
-    let age: i16 = current_year - year_of_birth;
+    let current_year: u16 = 2025;
+    let current_month: u16 = 1;
+    let current_day: u16 = 1;
+
+    let mut age: i16 = current_year - year_of_birth;
+
+    if birth_month > current_month {
+      age -= 1;
+    }
+    else if birth_day > current_day {
+      age -= 1;
+    }
 
     println!("The age of {} calculated to the year of {} is {}", name, year_of_birth, age);
 }
